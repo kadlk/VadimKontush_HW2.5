1e39468472dfeb83afece81b64bc1d3abb2d79c5
Vadim <119fps.com>
2021-11-03 16:17:11 +0300

    Change String in Dictionary to AnimalType

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -14,7 +14,7 @@ class ResultViewController: UIViewController {
     @IBOutlet var navigationInfoOutlet: UINavigationItem!
     
     var answers = [Answer?]()
-    var animalsTypeCount: [String: Int] = ["catCount": 0, "dogCount": 0, "rabbitCount": 0, "turtleCount": 0]
+    var animalsTypeCount: [AnimalType: Int] = [AnimalType.cat: 0, AnimalType.dog: 0, AnimalType.rabbit: 0, AnimalType.turtle: 0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,34 +23,34 @@ class ResultViewController: UIViewController {
         
         for answer in answers{
             if answer?.type == AnimalType.dog{
-                animalsTypeCount["dogCount", default: 0] += 1
+                animalsTypeCount[AnimalType.dog, default: 0] += 1
             }
             if answer?.type == AnimalType.cat{
-                animalsTypeCount["catCount", default: 0] += 1
+                animalsTypeCount[AnimalType.cat, default: 0] += 1
             }
             if answer?.type == AnimalType.rabbit{
-                animalsTypeCount["rabbitCount", default: 0] += 1
+                animalsTypeCount[AnimalType.rabbit, default: 0] += 1
             }
             if answer?.type == AnimalType.turtle{
-                animalsTypeCount["turtleCount", default: 0] += 1
+                animalsTypeCount[AnimalType.turtle, default: 0] += 1
             }
         }
         
         let greatestKey = (animalsTypeCount.max{a, b in a.value < b.value})?.key
         
-        if greatestKey == "dogCount"{
+        if greatestKey == AnimalType.dog{
             animalLabelOutlet.text = "Вы - \(AnimalType.dog.rawValue)" 
             descriptionLabelOutlet.text = AnimalType.dog.definition
         }
-        else if greatestKey == "catCount"{
+        else if greatestKey == AnimalType.cat{
             animalLabelOutlet.text = "Вы - \(AnimalType.cat.rawValue)"
             descriptionLabelOutlet.text = AnimalType.cat.definition
         }
-        else if greatestKey == "rabbitCount"{
+        else if greatestKey == AnimalType.rabbit{
             animalLabelOutlet.text = "Вы - \(AnimalType.rabbit.rawValue)"
             descriptionLabelOutlet.text = AnimalType.rabbit.definition
         }
-        else if greatestKey == "turtleCount"{
+        else if greatestKey == AnimalType.turtle{
             animalLabelOutlet.text = "Вы - \(AnimalType.turtle.rawValue)"
             descriptionLabelOutlet.text = AnimalType.turtle.definition
         }
